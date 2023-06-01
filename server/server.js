a8e5778fb41949c3648a39ed03083723687286cd
Jere Huttunen <jerehuttunen.fi@gmail.com>
2023-06-01 12:36:59 +0300

    Add Express.js

---
+++ server/server.js
@@ -0,0 +1,8 @@
+const app = require('./src');
+const http = require('http');
+const port = process.env.PORT || 3000;
+const server = http.createServer(app);
+
+server.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+})
