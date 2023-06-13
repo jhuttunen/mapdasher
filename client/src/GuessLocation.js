550ff3184b0c1fed3d8c4e808660c14c61476ee9
Jere Huttunen <jerehuttunen.fi@gmail.com>
2023-06-13 17:19:53 +0300

    Calculate round score based on distance

--- client/src/GuessLocation.js
+++ client/src/GuessLocation.js
@@ -24,7 +24,7 @@ let DefaultIcon = L.icon({
 L.Marker.prototype.options.icon = DefaultIcon;
 
 const GuessLocation = () => {
-  const [currentRound, setCurrentRound] = useState(0);
+  const [game, setGame] = useState({currentRound:0, totalScore:0});
   const [locations, setLocations] = useState([]);
   const [rounds, setRounds] = useState([]);
   const [guessMarker, setGuessMarker] = useState(null);
@@ -34,20 +34,25 @@ const GuessLocation = () => {
     setLocations([]);
     setGuessMarker(null);
     setRounds([]);
-    setCurrentRound(1);
+    setGame({currentRound:1, totalScore:0});
     fetchAndSetLocation('newGame');
   };
 
   // Handle submitting guess
   const submitGuess = () => {
     const round = {};
-    round.question = locations[currentRound-1];
+    round.question = locations[game.currentRound-1];
     round.answer = guessMarker;
-    round.number = currentRound;
-    const distance = calculateDistance(round.answer, {'lat': locations[currentRound-1].lat, 'lon': locations[currentRound-1].lon});
+    round.number = game.currentRound;
+    const distance = calculateDistance(round.answer, {'lat': locations[game.currentRound-1].lat, 'lon': locations[game.currentRound-1].lon});
     round.distance = distance.toFixed(0);
+    const score = calculateRoundScore(distance);
+    round.score = score;
     setRounds((prevState) => [...prevState, round]);
-    setCurrentRound(prevState => prevState+1);
+    setGame((prevState) => ({
+      ...prevState,
+      currentRound: prevState.currentRound + 1, totalScore: prevState.totalScore+score
+    }));
     setGuessMarker(null);
     fetchAndSetLocation(); // New round starts automatically atm
   };
@@ -58,7 +63,7 @@ const GuessLocation = () => {
       await fetch('http://localhost:4000/api/locations/1')
     ).json();
     let location = response[0];
-    location.round = currentRound;
+    location.round = game.currentRound;
     // Overwrite previous locations if new game
     (newGame) ? setLocations([location]) : setLocations([...locations, location]);
   };
@@ -66,6 +71,12 @@ const GuessLocation = () => {
   // Calculate distance between answer and target location with haversine
   const calculateDistance = (loc1, loc2) => {
     return haversine(loc1, loc2, {format:'{lon,lat}'}); 
+  };
+
+  // Calculate round score based on distance
+  const calculateRoundScore = (distance) => {
+    const score = (5000-distance).toFixed(0);
+    return  (score > 0) ? score : 0;
   };
 
   // Place and update guess marker location
@@ -99,10 +110,10 @@ const GuessLocation = () => {
 
   // Single guess
   const Guess = (props) => {
-    const {coordinates, city, country, distance, number} = props;
+    const {coordinates, city, country, distance, number, score} = props;
     return(
       <li key={coordinates}>
-        {number}: ({coordinates[0]}, {coordinates[1]}) was <b>{distance}</b> km from <b>{city}</b>, {country}
+        {number}: <b>{score}</b> points, ({coordinates[0]}, {coordinates[1]}) was <b>{distance}</b> km from <b>{city}</b>, {country}
       </li>
     );
   };
@@ -124,6 +135,7 @@ const GuessLocation = () => {
                   city={round.question.city}
                   country={round.question.country}
                   distance={round.distance}
+                  score={round.score}
                 />
               ))}
             </ul>
@@ -150,18 +162,19 @@ const GuessLocation = () => {
     <div style={{ display: 'flex' }}>
       <div style={{ flex: '0 0 400px', padding: '0 20px' }}>
         <GameControls 
-          currentRound={currentRound}
+          currentRound={game.currentRound}
           submitGuess={submitGuess}
           startNewGame={startNewGame}
         />
-        {currentRound > 0 && (
+        {game.currentRound > 0 && (
           <>
             <Question 
-              currentRound={currentRound}
+              currentRound={game.currentRound}
               locations={locations}
             />
             <GuessList 
-              rounds={rounds} 
+              rounds={rounds}
+              game={game} 
             />
             {locations[0] && ( // List of locations for debugging purposes
               <ul>
@@ -187,7 +200,7 @@ const GuessLocation = () => {
           {/* <TileLayer url='https://{s}.basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}.png' />*/}
           <TileLayer url="https://{s}.basemaps.cartocdn.com/light_nolabels/{z}/{x}/{y}.png" />
           <LocationPicker 
-            currentRound={currentRound}
+            currentRound={game.currentRound}
             guessMarker={guessMarker}
             setGuessMarker={setGuessMarker}
           />
@@ -203,7 +216,8 @@ const GuessLocation = () => {
                 <Marker position={[round.answer.lat, round.answer.lon]}>
                   <Tooltip permanent>
                     Guess {round.number}<br />
-                    <b>{round.distance} km</b> away
+                    Distance {round.distance} km<br />
+                    Score {round.score}
                   </Tooltip>
                 </Marker>
                 <Polyline positions={[[round.question.lat, round.question.lon], [round.answer.lat, round.answer.lon]]} />
