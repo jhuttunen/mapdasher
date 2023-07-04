dcc7f580e245f8d16d1757f03492df3f99725c20
Jere Huttunen <jerehuttunen.fi@gmail.com>
2023-07-04 16:51:34 +0300

    Updates

--- server/server.js
+++
@@ -1,8 +0,0 @@
-const app = require('./src');
-const http = require('http');
-const port = process.env.PORT || 8080;
-const server = http.createServer(app);
-
-server.listen(port, () => {
-  console.log(`Server running on port ${port}`);
-})
