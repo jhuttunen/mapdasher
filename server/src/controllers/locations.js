6240e34248c60f7439b8cac85167d0fe287b6215
Jere Huttunen <jerehuttunen.fi@gmail.com>
2023-07-27 12:57:49 +0300

    Add error handlers to locations controller

--- server/src/controllers/locations.js
+++ server/src/controllers/locations.js
@@ -1,86 +1,120 @@
 const prisma = require('../db/prisma');
 const { locations } = require('../data/capitals');
 
+// Get all continents
 const getContinents = async (req, res) => {
-  const result = await prisma.continent.findMany();
-  res.status(200).json(result);
+  try {
+    const result = await prisma.continent.findMany();
+    res.status(200).json(result);
+  } catch (e) {
+    res.status(500).json({message: "Something went wrong",})
+  }
 }
 
+// Get all countries
 const getCountries = async (req, res) => {
-  const result = await prisma.country.findMany();
-  res.status(200).json(result);
+  try {
+    const result = await prisma.country.findMany();
+    res.status(200).json(result);
+  } catch (e) {
+    res.status(500).json({message: "Something went wrong",})
+  }
 }
 
+// Get desired number of random cities from world, continent or country
 const getRandomCities = async (req, res) => {
   let { num, country, continent } = req.params;
   country = (country) ? country.toUpperCase() : '';
   continent = (continent) ? continent.toUpperCase() : '';
   num = (num > 0) ? ((num <= 50) ? num : 50) : 1;
   num = parseInt(num);
-
-  if(country){
-    const result = await prisma.$queryRaw`
-      SELECT * FROM cities 
-      WHERE iso2=${country}
-      ORDER BY random()
-      LIMIT ${num}
-    `;
-    res.status(200).json(result);
-  }else if(continent){
-    const result = await prisma.$queryRaw`
-      SELECT * FROM cities
-      LEFT JOIN countries ON iso2=country_code
-      WHERE continent=${continent}
-      ORDER BY RANDOM()
-      LIMIT ${num}
-    `;
-    res.status(200).json(result);
-  }else{
-    const result = await prisma.$queryRaw`
-      SELECT * from cities LIMIT ${num}
-      OFFSET floor(random() * (SELECT count(*) FROM cities));
-    `;
-    res.status(200).json(result);
+  try {
+    if(country){
+      const result = await prisma.$queryRaw`
+        SELECT * FROM cities 
+        WHERE iso2=${country}
+        ORDER BY random()
+        LIMIT ${num}
+      `;
+      res.status(200).json(result);
+    }else if(continent){
+      const result = await prisma.$queryRaw`
+        SELECT * FROM cities
+        LEFT JOIN countries ON iso2=country_code
+        WHERE continent=${continent}
+        ORDER BY RANDOM()
+        LIMIT ${num}
+      `;
+      res.status(200).json(result);
+    }else{
+      const result = await prisma.$queryRaw`
+        SELECT * from cities LIMIT ${num}
+        OFFSET floor(random() * (SELECT count(*) FROM cities));
+      `;
+      res.status(200).json(result);
+    }
+  } catch (e) {
+    res.status(500).json({message: "Something went wrong",})
   }
 }
 
+// Get all country codes
 const getCountryCodes = async (req, res) => {
-  const result = await prisma.country.findMany({
-    select: {
-      country_code: true,
-      country_name: true
-    }
-  });
-  res.status(200).json(result);
+  try {
+    const result = await prisma.country.findMany({
+      select: {
+        countryCode: true,
+        countryName: true
+      }
+    });
+    res.status(200).json(result);
+  } catch (e) {
+    res.status(500).json({message: "Something went wrong",})
+  }
 }
 
+// Get all capital cities
 const getCapitals = async (req, res) => {
-  const result = await prisma.city.findMany({
-    where: {
-      capital: 'primary'
-    }
-  });
-  res.status(200).json(result);
+  try {
+    const result = await prisma.city.findMany({
+      where: {
+        capital: 'primary'
+      }
+    });
+    res.status(200).json(result);
+  } catch (e) {
+    res.status(500).json({message: "Something went wrong",})
+  }
 }
 
+// Get all countries from a continent
 const getCountriesContinent = async (req, res) => {
   const { continent } = req.params;
-  const result = await prisma.country.findMany({
-    where: {
-      continent: continent.toUpperCase()
-    }
-  });
-  res.status(200).json(result);
+  try {
+    const result = await prisma.country.findMany({
+      where: {
+        continent: continent.toUpperCase()
+      }
+    });
+    res.status(200).json(result);
+  } catch (e) {
+    res.status(500).json({message: "Something went wrong",})
+  }
 }
 
+// Get all cities from a country
 const getCitiesCountry = async (req, res) => {
   const { country } = req.params;
-  const result = await prisma.city.findMany({
-    where: {
-      iso2: country.toUpperCase()
-    }
-  });
-  res.status(200).json(result);
+  try {
+    const result = await prisma.city.findMany({
+      where: {
+        iso2: country.toUpperCase()
+      }
+    });
+    res.status(200).json(result);
+  } catch (e) {
+    res.status(500).json({message: "Something went wrong",})
+  }
 }
 
 // Return number of random locations
